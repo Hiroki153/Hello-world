e9275e30118de7c4aceb3d1c91ba6814b21e1033
Rascal <namekooyazi@gmail.com>
2020-09-05 13:13:55 +0900

    Initial Commit

---
+++ new app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  new app
+//
+//  Created by 仲井宏紀 on 2020/09/05.
+//  Copyright © 2020 hiroki.nakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
